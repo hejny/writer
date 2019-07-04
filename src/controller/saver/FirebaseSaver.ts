98915dd228620c30bcaa397bc8027be100e5d32c
Pavol Hejny <me@pavolhejny.com>
2019-07-04 15:18:55 +0200

    Comment Firebase

--- src/controller/saver/FirebaseSaver.ts
+++ src/controller/saver/FirebaseSaver.ts
@@ -1,4 +1,30 @@
+/*
+
 import 'firebase/database';
+import { AbstractSaver } from './AbstractSaver';
+
+export class LocalStorageSaver<TAppState> extends AbstractSaver<TAppState> {
+    constructor(
+        firebaseDatabase: firebase.database.Database,
+        documentId: string,
+        createDefaultAppState: () => TAppState,
+    ) {
+        super();
+        // TODO: Is it here better to use super or this
+        super.hydrateAppStateHelper(() => {
+            const appModelSerialized = localStorage.getItem(localStorageKey);
+            if (!appModelSerialized) {
+                throw new Error(
+                    `In localStorage is not value ${localStorageKey}.`,
+                );
+            }
+            return JSON.parse(appModelSerialized);
+        }, createDefaultAppState);
+        /*super.watchAppState((appState) => {
+            localStorage.setItem(localStorageKey, JSON.stringify(appState));
+        });*/
+    }
+}
 
 /*import * as firebase from 'firebase/app';
 
